a802d3768f0a051f9b2cc5462c8b0892f7ed1f17
chaochaoxiaoshu <zhang.xin1@outlook.com>
2025-06-14 23:17:57 +0800

    UPDATE: 更新项目结构

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,80 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs with Tailwind',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/styles/global.css'],
-		}),
-	],
-	vite: {
-		plugins: [tailwindcss()],
-	},
-});
+  integrations: [
+    starlight({
+      title: 'Docs with Tailwind',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/chaochaoxiaoshu/learn-frontend',
+        },
+      ],
+      sidebar: [
+        {
+          label: '入门指南',
+          slug: 'getting-started',
+        },
+        {
+          label: 'JavaScript',
+          autogenerate: { directory: 'javascript' },
+        },
+        {
+          label: '框架',
+          items: [
+            {
+              label: 'Vue',
+              autogenerate: { directory: 'frameworks/vue' },
+            },
+            {
+              label: 'React',
+              autogenerate: { directory: 'frameworks/react' },
+            },
+          ],
+        },
+        {
+          label: '前端工程化',
+          items: [
+            {
+              label: 'ES Module',
+              slug: 'engineering/es-module',
+            },
+            {
+              label: '打包器',
+              slug: 'engineering/bundler',
+            },
+            {
+              label: '代码格式化工具',
+              slug: 'engineering/formatter',
+            },
+            {
+              label: '代码检查工具',
+              slug: 'engineering/linter',
+            },
+          ],
+        },
+        {
+          label: 'TypeScript',
+          autogenerate: { directory: 'typescript' },
+        },
+        {
+          label: '代码挑战',
+          autogenerate: { directory: 'challenges' },
+        },
+        {
+          label: '前端英语',
+          autogenerate: { directory: 'english' },
+        },
+      ],
+      customCss: ['./src/styles/global.css'],
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
